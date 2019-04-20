49ab149883bc6f0f8a593885da7a86dbe9db5313
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-20 17:13:55 -0400

    General PowerShell stuff.

---
+++ GeneralPowerShellExamples.ps1
@@ -0,0 +1,18 @@
+﻿<#
+
+PowerShell.exe Command-Line Help
+https://docs.microsoft.com/en-us/powershell/scripting/components/console/powershell.exe-command-line-help?view=powershell-6
+
+
+
+
+
+
+
+
+
+
+
+
+
+#>
