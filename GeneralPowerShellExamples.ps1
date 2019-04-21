3facbb52c7b1a53e1733f97ea6487120df2028a2
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-21 06:54:05 -0400

    Added comparison operators link

--- GeneralPowerShellExamples.ps1
+++ GeneralPowerShellExamples.ps1
@@ -3,7 +3,8 @@
 PowerShell.exe Command-Line Help
 https://docs.microsoft.com/en-us/powershell/scripting/components/console/powershell.exe-command-line-help?view=powershell-6
 
-
+Comparison Operataors
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-6
 
 
 
