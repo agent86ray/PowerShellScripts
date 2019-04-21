0cdaf11f36af4d3ee5807e59659945d51bae92f6
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-21 07:32:15 -0400

    Added content

--- GeneralPowerShellExamples.ps1
+++ GeneralPowerShellExamples.ps1
@@ -6,14 +6,18 @@ https://docs.microsoft.com/en-us/powershell/scripting/components/console/powersh
 Comparison Operataors
 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-6
 
+Get-ChildItem
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-6
 
+Get-Content
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-content?view=powershell-6
 
+New-Item
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item?view=powershell-6
 
-
-
-
-
-
-
+Get-Member
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-member?view=powershell-6
 
 #>
+
+Get-ChildItem -Path C:\ETL\FORMAT_FILES | Get-Member
