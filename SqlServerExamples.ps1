73dbbc596902b923c6cad7bcbbbeb001a3edd411
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-21 09:11:24 -0400

    Added Read-SQlTableData example

--- SqlServerExamples.ps1
+++ SqlServerExamples.ps1
@@ -6,7 +6,10 @@ Get-Command -Module SqlServer
 
 Get-Help Write-SqlTableData -Examples
 
+Get-Help Read-SqlTableData -Examples
+
 # create database, schema, table if it doesn't exist
 Get-ChildItem -Path C:\ETL\FORMAT_FILES | Select-Object DirectoryName, FullName, Extension, Name, Length,LastWriteTime | Write-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES -Force
 
+Read-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES |Export-Excel -Now
 
