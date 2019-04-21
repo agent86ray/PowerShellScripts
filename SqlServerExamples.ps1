331d76fb4ef09b35fe72fc6456cafa00421449ee
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-21 09:06:41 -0400

    Added Write-SQlTableData example.

--- SqlServerExamples.ps1
+++ SqlServerExamples.ps1
@@ -3,3 +3,10 @@
 Install-Module -Name SqlServer -AllowClobber
 
 Get-Command -Module SqlServer
+
+Get-Help Write-SqlTableData -Examples
+
+# create database, schema, table if it doesn't exist
+Get-ChildItem -Path C:\ETL\FORMAT_FILES | Select-Object DirectoryName, FullName, Extension, Name, Length,LastWriteTime | Write-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES -Force
+
+
