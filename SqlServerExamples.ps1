ec6dafbf3742134581c2f5bf53dafc966d526061
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-23 05:51:18 -0400

    wip

--- SqlServerExamples.ps1
+++ SqlServerExamples.ps1
@@ -1,8 +1,12 @@
 ﻿<# SqlServer #>
 
-Install-Module -Name SqlServer -AllowClobber
+Install-Module -Name SqlServer -Force -AllowClobber
 
-Get-Command -Module SqlServer
+Update-Module -Name SqlServer -WhatIf
+
+Get-Command -Module SqlServer -Noun *JoB* | Export-Excel -Now
+
+Get-Command -Module SqlServer | Out-GridView
 
 Get-Help Write-SqlTableData -Examples
 
@@ -11,5 +15,12 @@ Get-Help Read-SqlTableData -Examples
 # create database, schema, table if it doesn't exist
 Get-ChildItem -Path C:\ETL\FORMAT_FILES | Select-Object DirectoryName, FullName, Extension, Name, Length,LastWriteTime | Write-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES -Force
 
-Read-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES |Export-Excel -Now
+Read-SQlTableData -ServerInstance localhost -DatabaseName ETL -SchemaName dbo -TableName FORMAT_FILES | Export-Excel -Now
+
+DIR -recurse 'SQLSERVER:\SQLRegistration\Database Engine Server Group'
+
+$instance = Get-SqlInstance -ServerInstance localhost
+$instance.Configuration.Properties
+
+Set-SqlErrorLog
 
