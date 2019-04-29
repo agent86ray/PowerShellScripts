049b0d25d1a56e85be0c4b24a7a74003ae0fd795
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-04-29 06:06:18 -0400

    Create format file using PowerShell variable for -f parameter.

--- BCP_Examples.ps1
+++ BCP_Examples.ps1
@@ -1,16 +1,24 @@
 $parameters = @{ 
     bcpPath = 'C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\bcp.exe'
-    outputFolder = "C:\OUTBOUND"
+    formatFilesFolder = "C:\DATA_CONVERSION_XG\DEV\FORMAT_FILES"
+    exportFilesFolder = "C:\DATA_CONVERSION_XG\DEV\EXPORT_FILES"
 }
 
 $bcpPath = $parameters.Item("bcpPath")
 $bcpPath
 
-$outputFolder = $parameters.Item("outputFolder")
-$outputFolder
+$formatFilesFolder = $parameters.Item("formatFilesFolder")
+$formatFilesFolder
 
-bcp
+$exportFilesFolder = $parameters.Item("exportFilesFolder")
+$exportFilesFolder
 
-bcp ETL.dbo.CONTACT format nul -x -c -t"" -f C:\DATA_CONVERSION_XG\DEV\FORMAT_FILES\CONTACT_fmt.xml -T -S localhost
+$formatFile = "$formatFilesFolder\CONTACT_fmt.xml"
 
+bcp ETL.dbo.CONTACT format nul -x -c -t"" -f $formatFile -T -S localhost
+
+#export to fixed width flat file
 bcp ETL.dbo.CONTACT out C:\DATA_CONVERSION_XG\DEV\EXPORT_FILES\CONTACT.txt -f C:\DATA_CONVERSION_XG\DEV\FORMAT_FILES\CONTACT_fmt.xml -T -S localhost
+
+# import from fixed width flat file
+bcp ETL.dbo.CONTACT in C:\DATA_CONVERSION_XG\DEV\EXPORT_FILES\CONTACT.txt -f C:\DATA_CONVERSION_XG\DEV\FORMAT_FILES\CONTACT_fmt.xml -T -S localhost
