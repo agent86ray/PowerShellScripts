4732df92afaa45fd745a34f31ba504f39d8d743e
agent86ray <45230312+agent86ray@users.noreply.github.com>
2019-05-12 11:11:38 -0400

    Added BCP command examples

--- BCP Fixed Width File.ps1
+++ BCP Fixed Width File.ps1
@@ -12,5 +12,11 @@ C:\ETL\FORMAT_FILES\FORMAT_FILE_NAME.xml
 
 #>
 
-
+<# create BCP format for fixed-width file (no delimiters) #>
 BCP ETL.dbo.CONTACT format nul -c -t"" -f C:\ETL\FORMAT_FILES\CONTACT.xml -x -T -S localhost
+
+<# export table to fixed-width file #>
+BCP ETL.dbo.CONTACT out C:\ETL\EXPORT_FILES\CONTACT.txt -f C:\ETL\FORMAT_FILES\CONTACT.xml -T -S localhost
+
+<# import fixed-width file #>
+BCP ETL.import.CONTACT in C:\ETL\EXPORT_FILES\CONTACT.txt -f C:\ETL\FORMAT_FILES\CONTACT.xml -T -S localhost
